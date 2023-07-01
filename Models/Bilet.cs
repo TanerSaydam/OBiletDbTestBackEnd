6276e701d536ce0acc757c33e9a8544361ea390d
tanersaydam <tanersaydam@gmail.com>
2023-07-01 19:46:15 +0300

    .

--- Models/Bilet.cs
+++ Models/Bilet.cs
@@ -5,6 +5,7 @@ namespace OBiletDbTestBackEnd.Models;
 public sealed class Bilet
 {
     public int Id { get; set; }
+    public string BiletNumarasi { get; set; } //GQ1235
     public int KoltukNumarasi { get; set; }
 
     [ForeignKey("Yolcu")]
