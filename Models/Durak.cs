7b786b75ae5f36ce4d9cb338f0a11a055124064c
tanersaydam <tanersaydam@gmail.com>
2023-07-01 19:44:26 +0300

    .

---
+++ Models/Durak.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OBiletDbTestBackEnd.Models;
+
+public sealed class Durak
+{
+    public int Id { get; set; }
+    public string SehirAdi { get; set; }
+    public bool BaslangicDuragiMi { get; set; }
+    public bool BitisDuragiMi { get; set; }
+    public string KalkisYeri { get; set; }
+    public DateTime KalkisTarihi { get; set; }
+    public DateTime VarisTarihi { get; set; }
+
+    [ForeignKey("Sefer")]
+    public int SeferId { get; set; }
+    public Sefer Sefer { get; set; }
+}
--- Models/Sefer.cs
+++ Models/Sefer.cs
@@ -5,11 +5,7 @@ namespace OBiletDbTestBackEnd.Models;
 public sealed class Sefer
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string GidilecekYer { get; set; }
-    public string KalkisYeri { get; set; }
-    public DateTime KalkisTarihi { get; set; }
-    public DateTime VarisTarihi { get; set; }
+    public string Name { get; set; }    
 
     [ForeignKey("Otobus")]
     public int OtobusId { get; set; }
@@ -18,4 +14,5 @@ public sealed class Sefer
     public ICollection<SeferSofor> SeferSofors { get; set; }
     public ICollection<SeferMuavin> SeferMuavins { get; set; }
     public ICollection<Bilet> Bilets { get; set; }
+    public ICollection<Durak> Duraks { get; set; }
 }
